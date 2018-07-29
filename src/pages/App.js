560c776058efc94ce49b0ff5656ca1ac523a95cc
Daniel Greuel <dgreuel@gmail.com>
2018-07-29 02:31:16 -0500

    styling, animations

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,14 +8,15 @@ export default ({ children }) => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
     </header>
-    <nav class="mb-1 navbar navbar-expand-lg navbar-dark ">
-      <a class="navbar-brand">
-        {' '}
-        <IndexLink to="/"> Home </IndexLink>
-      </a>
-      <a class="navbar-brand">
-        <Link to="/about"> About </Link>
-      </a>
+    <nav className="mb-1 navbar navbar-expand-lg navbar-dark ">
+      <ul>
+        <li className="navbar-brand">
+          <IndexLink to="/"> Home </IndexLink>
+        </li>
+        <li className="navbar-brand">
+          <Link to="/about"> About </Link>
+        </li>
+      </ul>
     </nav>
 
     <div className="Body">{children}</div>
